d0aa934fc38cbae438f230bc6f87b78f4b4c9313
macs <macs@macbook.com>
2019-12-14 15:02:01 +0530

    user login in touch rfid

--- app/bodegaBox/swipeCard.js
+++ app/bodegaBox/swipeCard.js
@@ -3,26 +3,34 @@ exports.cardSignIn = function(BodegaBox){
     BodegaBox.touchRfidcard = function(id,tagId,cb) {
         var User = BodegaBox.app.models.user
         var Cart = BodegaBox.app.models.Cart
-        User.findOne({fields: {rfidTagId: tagId}}, function(err,instance){
-            if(instance != null) {
-                console.log(instance)
-                cb(null, instance)
-            } else if (err){
-                cb(err,null)
-            } else {
-                var error = new Error();
-                error.status = 500;
-                error.message = 'Could not find user related to RFID card'; 
-                cb(error, null)
+
+        User.login(
+            {
+                username: tagId,
+                password: tagId
+            }, 'user', function(err, token) {
+                if (err) {
+                    cb(err,null)
+                    return;
+                }
+                if (token != null) {
+                    cb(null, {"token": token})
+                    return
+                } else {
+                    var error = new Error();
+                    error.status = 500;
+                    error.message = 'Could not find user related to RFID card'; 
+                    cb(error, null)
+                }
             }
-        })
+        );
     }
 
     BodegaBox.remoteMethod('touchRfidcard', {
         description: 'Blog details api with prevoius. next and related blogs',
         accepts: [
             {arg: 'id', http: 'path', type: 'string'},
-            {arg: 'tagId', type: 'string', required: true}
+            {arg: 'tagId', type: 'string', required: true},
         ],
         returns: {
           arg: 'user',
